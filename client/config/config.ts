8b0759ac067d1eae97e4cd6b1c006df11faee4e5
Govinda Mandal <govinda4india@gmail.com>
2024-07-19 23:27:50 +0530

    Created client and server

---
+++ client/config/config.ts
@@ -0,0 +1,15 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyCcEapp0hdkBOieBXemw2adny_4UtB5G6o",
+        authDomain: "blog-40bf2.firebaseapp.com",
+        projectId: "blog-40bf2",
+        storageBucket: "blog-40bf2.appspot.com",
+        messagingSenderId: "516566795950",
+        appId: "1:516566795950:web:03f4b6c90cde672e58cf3f"
+    },
+    server: {
+        url: 'http://localhost:1337'
+    }
+};
+
+export default config;
